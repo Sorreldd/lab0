3a972e59ec9cdb0331ec838ceb8b0792a76d4590
Artyom Komarov <artyom.sorrel@gmail.com>
2016-09-17 16:22:26 +0300

    lab0

---
+++ src/main/java/lab0.java
@@ -0,0 +1,13 @@
+import java.util.DoubleSummaryStatistics;
+
+/**
+ * Created by kd on 17.09.2016.
+ */
+public class lab0 {
+
+    public static void main(String[] args) {
+        Double x;
+        x.o()
+        Double.compare()
+    }
+}
